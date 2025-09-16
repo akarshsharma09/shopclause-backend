dcb0fa5a009ff632b53bd1c6f0dbdd37d3474246
Akarsh Sharma <76594459+akarshsharma09@users.noreply.github.com>
2025-09-16 16:26:49 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -16,14 +16,14 @@ if (process.env.NODE_ENV !== "production") {
 
 // Create Sequelize instance
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || process.env.MYSQLDATABASE,
+  process.env.DB_USER || process.env.MYSQLUSER,
+  process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
     dialect: process.env.DB_DIALECT || "mysql",
-    logging: false, // disable SQL logs
+    logging: false,
   }
 );
 
