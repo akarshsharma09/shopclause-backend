4f07a6267091aedb599a84c39e9ab0da3774a34f
Akarsh Sharma <76594459+akarshsharma09@users.noreply.github.com>
2025-09-18 16:51:56 +0530

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -10,7 +10,12 @@ const Product = sequelize.define("Product", {
   stock: { type: DataTypes.INTEGER, defaultValue: 0 },
   image: { type: DataTypes.STRING },
   deal_price: DataTypes.DECIMAL(10,2),
-});
+},
+  {
+    tableName: "products", // 👈 Force lowercase table name
+    timestamps: false,
+  }
+                                );
 
 // Relation: ek category ke under multiple products
 // Category.hasMany(Product, { foreignKey: "categoryId" , onDelete: "CASCADE" });
