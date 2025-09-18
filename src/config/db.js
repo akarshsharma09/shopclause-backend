b568cec43e2d4cd896a501d3982da50d5e972eb1
Akarsh Sharma <76594459+akarshsharma09@users.noreply.github.com>
2025-09-18 09:32:13 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,28 +3,29 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import path from "path";
 
-// Load .env in local dev only
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: path.resolve(".env") });
 }
 
-// Debug logs
-console.log("Using DB_HOST:", process.env.DB_HOST);
-console.log("Using DB_PORT:", process.env.DB_PORT);
-console.log("Using DB_NAME:", process.env.DB_NAME);
-console.log("Using DB_USER:", process.env.DB_USER);
+const DB_NAME = process.env.DB_NAME || process.env.MYSQLDATABASE;
+const DB_USER = process.env.DB_USER || process.env.MYSQLUSER;
+const DB_PASSWORD = process.env.DB_PASSWORD || process.env.MYSQLPASSWORD;
+const DB_HOST = process.env.DB_HOST || process.env.MYSQLHOST;
+const DB_PORT = process.env.DB_PORT || process.env.MYSQLPORT;
+const DIALECT = "mysql";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+// Debug logs
+console.log("Using DB_HOST:", DB_HOST);
+console.log("Using DB_PORT:", DB_PORT);
+console.log("Using DB_NAME:", DB_NAME);
+console.log("Using DB_USER:", DB_USER);
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: DIALECT,
+  logging: false,
+});
 
 const connectDB = async () => {
   let retries = 5;
